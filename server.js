fae7e29d38a0754f8db16b2d0b07c8d87ef9e3b5
Tan Luat <luatb1910254@student.ctu.edu.vn>
2023-02-24 08:45:09 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const app = require('./app');
 const config = require('./app/config');
+const MongoDB = require("./app/utils/mongodb.util");
+
+async function startServer() {
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log("connected to the database!");
+
+        const PORT = config.app.port;
+        app.listen(3005, () => {
+            console.log('Server is running on port ${PORT} ');
+        });
+    } catch (error) {
+        console.log("Cannot connect to the database!", error);
+        process.exit();
+    }
+}
+startServer();
 
 //start server
 const PORT = config.app.port;
